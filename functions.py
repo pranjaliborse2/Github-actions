ca7c71cc51445e61cac63ef04dfd1f0a06ed4510
pranjaliborse2 <pranjaliborse21999@gmail.com>
2025-08-16 19:26:07 -0500

    function and test file added

---
+++ functions.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+def add(a,b):
+    return a+b
+
+def divide(a,b):
+    return b/a
+
+def subtract(a,b):
+    return a-b
---
+++ test_unittest.py
@@ -0,0 +1,26 @@
+
+from functions import add, subtract, divide
+import numpy as np
+import unittest
+
+# def test_func_add():
+#     assert add(0, np.inf) == np.inf 
+
+# def test_func_subtract():
+#     assert subtract(np.inf,100000000000000000000) == np.inf and subtract(100000000000000000000,-np.inf) == np.inf
+
+# def test_func_divide():
+#     assert divide(1,0) == 0
+
+class test_functions(unittest.TestCase):
+    def test_func_add(self):
+        self.assertEqual(add(0, np.inf), np.inf)
+
+    def test_func_subtract(self):
+        self.assertEqual(subtract(np.inf,100000000000000000000), np.inf) and self.assertEqual(subtract(100000000000000000000,-np.inf),np.inf)
+
+    def test_func_divide(self):
+        self.assertEqual(divide(1,0),0)
+
+if __name__=='__main__':
+    unittest.main()
